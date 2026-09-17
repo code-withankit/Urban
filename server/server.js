58f849537e2747c89a732be4023d8a1e695739bf
Ankit <contact@codewithankit.dev>
2026-09-17 21:16:30 +0530

    setup : server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send(`Server is running on PORT ${PORT}`);
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on PORT', PORT);
+})
